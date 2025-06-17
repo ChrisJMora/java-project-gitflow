9b68b65b76a5534360d73e7d1e3c0f72dd7c038e
Christian Jácome <129555977+ChrisJMora@users.noreply.github.com>
2025-06-17 02:32:50 +0000

    Corregir el saludo

--- Saludo.java
+++ Saludo.java
@@ -1,5 +1,8 @@
 public class Saludo {
     public String generarSaludo(String nombre, String apellido) {
+        if (nombre == null || nombre.isEmpty() || apellido.isEmpty()) {
+            return "Hola, Invitado!";
+        }
         return "Hola!, " + nombre + " " + apellido;
     }
 }
